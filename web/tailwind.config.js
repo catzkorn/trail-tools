9c7a1dcbe86329c3b0978788dac13c10de9f75a4
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2024-12-29 21:05:23 -0800

    Make tailwind use esmodule export

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,8 @@
-module.exports = {
-    content: ["./dist/index.{js,html}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./dist/index.{js,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
