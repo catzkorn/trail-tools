8870f05be316274453bfdc2a8382a83b42c426f0
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2024-12-29 20:30:58 -0800

    web: fix linter errors

--- web/index.tsx
+++ web/index.tsx
@@ -1,10 +1,14 @@
 // src/index.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const rootDiv = document.getElementById("root");
+if (rootDiv === null) {
+  throw new Error("Root div not found");
+}
+const root = ReactDOM.createRoot(rootDiv);
 
 root.render(
   <React.StrictMode>
