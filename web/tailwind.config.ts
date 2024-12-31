8cc16170a23b0514dcb8801f307185060baaf84b
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2024-12-30 20:30:26 -0800

    web: rename tailwind config file

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
   content: ["./dist/index.{js,html}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+} satisfies Config;
