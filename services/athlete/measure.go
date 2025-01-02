6ea7f1f8c4c2c407a37b40527e320630045cc50b
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2025-01-01 17:58:12 -0800

    proto: use new Go opaque API

--- services/athlete/measure.go
+++ services/athlete/measure.go
@@ -17,31 +17,31 @@ func (s *Service) CreateBloodLactateMeasure(ctx context.Context, req *connect.Re
 	if !ok {
 		return nil, connect.NewError(connect.CodeUnauthenticated, fmt.Errorf("unauthenticated"))
 	}
-	if req.Msg.HeartRateBpm <= 0 {
-		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("invalid heart rate: %d", req.Msg.HeartRateBpm))
+	if req.Msg.GetHeartRateBpm() <= 0 {
+		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("invalid heart rate: %d", req.Msg.GetHeartRateBpm()))
 	}
-	activityID, err := store.StringToUUID(req.Msg.ActivityId)
+	activityID, err := store.StringToUUID(req.Msg.GetActivityId())
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("invalid activity ID: %w", err))
 	}
-	mmol, err := decimal.NewFromString(req.Msg.MmolPerLiter)
+	mmol, err := decimal.NewFromString(req.Msg.GetMmolPerLiter())
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("invalid mmol per liter: %w", err))
 	}
 	if mmol.LessThan(decimal.Zero) {
-		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("mmol per liter must be positive: %s", req.Msg.MmolPerLiter))
+		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("mmol per liter must be positive: %s", req.Msg.GetMmolPerLiter()))
 	}
-	measure, err := s.athletes.AddMeasure(ctx, activityID, mmol, req.Msg.HeartRateBpm)
+	measure, err := s.athletes.AddMeasure(ctx, activityID, mmol, req.Msg.GetHeartRateBpm())
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to add measure: %w", err))
 	}
-	return connect.NewResponse(&athletesv1.CreateBloodLactateMeasureResponse{
-		BloodLactateMeasure: &athletesv1.BloodLactateMeasure{
+	return connect.NewResponse(athletesv1.CreateBloodLactateMeasureResponse_builder{
+		BloodLactateMeasure: athletesv1.BloodLactateMeasure_builder{
 			MmolPerLiter: measure.MmolPerLiter.String(),
 			HeartRateBpm: int32(measure.HeartRateBpm),
 			CreateTime:   timestamppb.New(measure.CreateTime.Time),
 			Id:           store.UUIDToString(measure.ID),
 			ActivityId:   store.UUIDToString(measure.ActivityID),
-		},
-	}), nil
+		}.Build(),
+	}.Build()), nil
 }
