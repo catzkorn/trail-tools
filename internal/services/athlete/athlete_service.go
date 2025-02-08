ef835153be57e0e2dd7f37f2bda77bd8419a73cc
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2025-02-07 22:15:26 -0800

    internal/service/athlete: remove oidc dependency

--- internal/services/athlete/athlete_service.go
+++ internal/services/athlete/athlete_service.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/catzkorn/trail-tools/internal/athletes"
 	"github.com/catzkorn/trail-tools/internal/gen/athletes/v1/athletesv1connect"
-	"github.com/catzkorn/trail-tools/internal/users"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/shopspring/decimal"
 )
@@ -19,22 +18,16 @@ type AthleteRepository interface {
 	AddMeasure(ctx context.Context, activityID pgtype.UUID, mmolPerLiter decimal.Decimal, heartRateBPM int32) (*athletes.BloodLactateMeasure, error)
 }
 
-type UserRepository interface {
-	GetOIDCUser(ctx context.Context, oidcSubject string) (*users.OIDCUser, error)
-}
-
 // Service implements API handlers for the athlete service.
 type Service struct {
 	log      *slog.Logger
-	users    UserRepository
 	athletes AthleteRepository
 }
 
 // NewService creates a new Service from the provided logger and directory.
-func NewService(log *slog.Logger, users UserRepository, athletes AthleteRepository) *Service {
+func NewService(log *slog.Logger, athletes AthleteRepository) *Service {
 	return &Service{
 		log:      log,
-		users:    users,
 		athletes: athletes,
 	}
 }
