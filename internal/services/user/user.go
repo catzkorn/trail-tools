1caf4c4c3dcb3dd25a715dcbeddaf055ee0cc682
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2025-02-07 22:21:55 -0800

    internal/services/user: support OIDC and WebAuthn users

--- internal/services/user/user.go
+++ internal/services/user/user.go
@@ -2,30 +2,45 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	"connectrpc.com/connect"
+	"github.com/catzkorn/trail-tools/internal/authn"
 	usersv1 "github.com/catzkorn/trail-tools/internal/gen/users/v1"
 	"github.com/catzkorn/trail-tools/internal/oidc"
 	"github.com/catzkorn/trail-tools/internal/store"
+	"github.com/catzkorn/trail-tools/internal/users"
 )
 
 func (s *Service) GetCurrentUser(ctx context.Context, req *connect.Request[usersv1.GetCurrentUserRequest]) (*connect.Response[usersv1.GetCurrentUserResponse], error) {
-	userInfo, ok := oidc.GetUserInfo(ctx)
+	user, ok := authn.GetUser(ctx)
 	if !ok {
 		return nil, connect.NewError(connect.CodeUnauthenticated, nil)
 	}
-	user, err := s.users.GetOIDCUser(ctx, userInfo.Subject)
-	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, err)
+	switch u := user.(type) {
+	case *users.OIDCUser:
+		userInfo, ok := oidc.GetUserInfo(ctx)
+		if !ok {
+			return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("no OIDC user info found"))
+		}
+		return connect.NewResponse(usersv1.GetCurrentUserResponse_builder{
+			User: usersv1.User_builder{
+				Id:         store.UUIDToString(user.ID()),
+				Email:      userInfo.Email,
+				Name:       userInfo.Name,
+				GivenName:  userInfo.GivenName,
+				FamilyName: userInfo.FamilyName,
+				AvatarUrl:  userInfo.AvatarURL,
+			}.Build(),
+		}.Build()), nil
+	case *users.WebAuthnUser:
+		return connect.NewResponse(usersv1.GetCurrentUserResponse_builder{
+			User: usersv1.User_builder{
+				Id:   store.UUIDToString(user.ID()),
+				Name: u.WebAuthnName(),
+			}.Build(),
+		}.Build()), nil
+	default:
+		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("unknown user type: %T", u))
 	}
-	return connect.NewResponse(usersv1.GetCurrentUserResponse_builder{
-		User: usersv1.User_builder{
-			Id:         store.UUIDToString(user.ID),
-			Email:      userInfo.Email,
-			Name:       userInfo.Name,
-			GivenName:  userInfo.GivenName,
-			FamilyName: userInfo.FamilyName,
-			AvatarUrl:  userInfo.AvatarURL,
-		}.Build(),
-	}.Build()), nil
 }
