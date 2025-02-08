1664caa558cf995d966268ddbebd60dec1740243
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2025-02-07 22:32:52 -0800

    web: add support for WebAuthn login

--- web/components/UserNav.tsx
+++ web/components/UserNav.tsx
@@ -35,7 +35,7 @@ const UserNav: React.FC<UserNavProps> = ({ user }) => {
           </MenuItem>
           <MenuItem key="logout">
             <a
-              href="/oidc/logout"
+              href="/logout"
               className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
             >
               Sign out
---
+++ web/components/WebAuthnForm.tsx
@@ -0,0 +1,140 @@
+import { Button, Input } from "@headlessui/react";
+import { KeyIcon } from "@heroicons/react/24/outline";
+import { base64URLStringToBuffer } from "helpers/base64URL";
+import React, { ChangeEvent, useEffect, useState, useTransition } from "react";
+import SignInButton from "./SignInButton";
+
+interface WebAuthnResponse {
+  publicKey: PublicKey;
+}
+
+interface PublicKey {
+  pubKeyCredParams: PublicKeyCredentialParameters[];
+  rp: PublicKeyCredentialRpEntity;
+  user: User;
+  challenge: string;
+
+  authenticatorSelection?: AuthenticatorSelectionCriteria;
+  excludeCredentials?: PublicKeyCredentialDescriptor[];
+  timeout?: number;
+  hints?: string[];
+  attestation?: AttestationConveyancePreference;
+  attestationFormats?: string[];
+  extensions?: object;
+}
+
+interface User {
+  name: string;
+  displayName: string;
+  id: string;
+}
+
+interface WebAuthnFormProps {
+  closeModal: () => void;
+}
+
+const WebAuthnForm: React.FC<WebAuthnFormProps> = ({ closeModal }) => {
+  const [isPending, startTransition] = useTransition();
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const triggerPasskeyRetrieval = async () => {
+      try {
+        const resp = await fetch("/webauthn/login/begin");
+        const data = (await resp.json()) as WebAuthnResponse;
+        const cred = await navigator.credentials.get({
+          publicKey: {
+            ...data.publicKey,
+            challenge: base64URLStringToBuffer(data.publicKey.challenge),
+          },
+          // Note: this component is only rendered if conditional mediation is available.
+          mediation: "conditional",
+        });
+        if (cred === null) {
+          console.error("failed to retrieve credential");
+          return;
+        }
+        await fetch("/webauthn/login/finish", {
+          body: JSON.stringify(cred),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        });
+        closeModal();
+        // Reaload the window to reload with the session cookie set
+        window.location.reload();
+      } catch (err: unknown) {
+        if (err instanceof DOMException) {
+          // Ignore AbortErrors, they seem to happen every time we load this.
+          if (err.name !== "AbortError") {
+            console.error(err, err.name, err.message);
+          }
+        } else {
+          console.error(err);
+        }
+      }
+    };
+    triggerPasskeyRetrieval().catch((err: unknown) => {
+      console.error("Failed to trigger passkey retrieval:", err);
+    });
+  });
+
+  const handleSubmit = () => {
+    startTransition(async () => {
+      if (username === "") {
+        return;
+      }
+      try {
+        const resp = await fetch("/webauthn/register/begin?name=" + username);
+        const data = (await resp.json()) as WebAuthnResponse;
+        const cred = await navigator.credentials.create({
+          publicKey: {
+            ...data.publicKey,
+            user: {
+              ...data.publicKey.user,
+              id: base64URLStringToBuffer(data.publicKey.user.id),
+            },
+            challenge: base64URLStringToBuffer(data.publicKey.challenge),
+          },
+        });
+        if (cred === null) {
+          console.error("failed to create credential");
+          return;
+        }
+        await fetch("/webauthn/register/finish", {
+          body: JSON.stringify(cred),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        });
+        closeModal();
+        // Reload the window to reload with the session cookie set
+        window.location.reload();
+      } catch (err) {
+        console.error(err);
+      }
+    });
+  };
+  return (
+    <div>
+      <form className="flex flex-col gap-2">
+        <Input
+          type="text"
+          placeholder="Enter your username"
+          className="block w-full rounded-md border border-button-border-light rounded-md bg-white px-3 py-1.5 text-base text-gray-900"
+          autoComplete="username webauthn"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <Button onClick={handleSubmit} disabled={isPending}>
+          <SignInButton logo={<KeyIcon />} text="Register with a passkey" />
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default WebAuthnForm;
