fc7ef09df60ba2205879b74f769e307eb5872b1c
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2025-02-08 20:02:38 -0800

    web: refactor App entrypoint

--- web/pages/HomePage.tsx
+++ web/pages/HomePage.tsx
@@ -1,21 +1,10 @@
-import Header from "@components/Header";
 import HomeContent from "@components/HomeContent";
-import { User } from "gen/users/v1/users_pb";
 import React from "react";
 
-interface HomePageProps {
-  user: User | null;
-}
-
-const HomePage: React.FC<HomePageProps> = ({ user }) => {
+const HomePage: React.FC = () => {
   return (
-    <div className="min-h-full">
-      <Header user={user} />
-      <main>
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <HomeContent />
-        </div>
-      </main>
+    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <HomeContent />
     </div>
   );
 };
