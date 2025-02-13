491b0a2798a13bd7bcb13976e11e8675cd00314b
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2025-02-12 21:59:52 -0800

    internal: add ListAthletes, DeleteAthlete

--- internal/athletes/athlete.go
+++ internal/athletes/athlete.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/catzkorn/trail-tools/internal/athletes/internal"
+	"github.com/catzkorn/trail-tools/internal/store"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
@@ -25,12 +26,30 @@ func (d *Repository) AddAthlete(ctx context.Context, name string, userID pgtype.
 	}, nil
 }
 
-func (d *Repository) DeleteAthlete(ctx context.Context, id pgtype.UUID) (*Athlete, error) {
-	athlete, err := d.q.DeleteAthlete(ctx, id)
+func (d *Repository) ListAthletesForUser(ctx context.Context, userID pgtype.UUID) ([]*Athlete, error) {
+	athletes, err := d.q.ListAthletesForUser(ctx, userID)
 	if err != nil {
-		return nil, fmt.Errorf("failed to delete athlete: %w", err)
+		return nil, fmt.Errorf("failed to list athletes: %w", err)
 	}
-	return &Athlete{
-		Athlete: athlete,
-	}, nil
+	var athleteResponses []*Athlete
+	for _, athlete := range athletes {
+		athleteResponses = append(athleteResponses, &Athlete{
+			Athlete: athlete,
+		})
+	}
+	return athleteResponses, nil
+}
+
+func (d *Repository) DeleteAthleteForUser(ctx context.Context, userID pgtype.UUID, athleteID pgtype.UUID) error {
+	numRows, err := d.q.DeleteAthleteForUser(ctx, &internal.DeleteAthleteForUserParams{
+		ID:     athleteID,
+		UserID: userID,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete athlete: %w", err)
+	}
+	if numRows == 0 {
+		return store.ErrNotFound
+	}
+	return nil
 }
